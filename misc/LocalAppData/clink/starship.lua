bc107ddfeaf64a54399465c67c5ffacd6413b6eb
sgur <255021+sgur@users.noreply.github.com>
2023-03-30 01:08:10 +0900

    chore: remove misc (backup) files

--- misc/LocalAppData/clink/starship.lua
+++
@@ -1,6 +0,0 @@
--- starship.lua
-
-local config_dir = path.join(os.getenv("USERPROFILE"), ".config")
-os.setenv("STARSHIP_CONFIG", path.join(config_dir, "starship_windows.toml"))
-
-load(io.popen('starship init cmd'):read("*a"))()
