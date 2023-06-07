b5ad3cb712e2eae24eb2bb9ae5a3540eb9295044
sgur <255021+sgur@users.noreply.github.com>
2023-06-07 13:39:02 +0900

    Update .config/fish/conf.d/20-pyenv.fish

--- dot_config/fish/conf.d/20-pyenv.fish
+++ dot_config/fish/conf.d/20-pyenv.fish
@@ -1,3 +1,9 @@
 if type -q pyenv
     pyenv init - | source
 end
+
+if test -x ~/.pyenv/bin/pyenv
+    set -gx PYENV_ROOT $HOME/.pyenv
+    fish_add_path -g $PYENV_ROOT/bin
+    pyenv init - | source
+end
