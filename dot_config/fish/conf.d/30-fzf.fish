7a256898b39f0de377dc28bfe2e253a477d00378
sgur <255021+sgur@users.noreply.github.com>
2023-06-07 18:45:02 +0900

    Update .config/fish/conf.d/30-fzf.fish

--- dot_config/fish/conf.d/30-fzf.fish
+++ dot_config/fish/conf.d/30-fzf.fish
@@ -1,17 +1,15 @@
 status is-interactive || exit
 
-if status is-login
-    if test -n "$TMUX"; and type -q fzf-tmux
-        # https://raw.githubusercontent.com/tmux/tmux/3.2/CHANGES
-        # tmux 3.2 以降で popup が利用可能
-        if test (string match --regex '\d(?:\.\d+)?' (tmux -V)) -ge 3.2
-            set -gx FZF_CMD fzf-tmux -p 90%,40%
-        else
-            set -gx FZF_CMD fzf-tmux -d 40%
-        end
+if test -n "$TMUX"; and type -q fzf-tmux
+    # https://raw.githubusercontent.com/tmux/tmux/3.2/CHANGES
+    # tmux 3.2 以降で popup が利用可能
+    if test (string match --regex '\d(?:\.\d+)?' (tmux -V)) -ge 3.2
+        set -gx FZF_CMD fzf-tmux -p 90%,40%
     else
-        set -gx FZF_CMD fzf --height=40%
+        set -gx FZF_CMD fzf-tmux -d 40%
     end
+else
+    set -gx FZF_CMD fzf --height=40%
 end
 
 # fisher plugin
