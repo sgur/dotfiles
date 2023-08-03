edc9b1c3c29d37e8d03310017e8be770218471ae
sgur <255021+sgur@users.noreply.github.com>
2023-08-03 14:08:21 +0900

    Update .config/fish/functions/fzf_ghq_repository_select.fish Update .config/fish/functions/fzf_git_branch_select.fish

--- dot_config/fish/functions/fzf_ghq_repository_select.fish
+++ dot_config/fish/functions/fzf_ghq_repository_select.fish
@@ -1,12 +1,12 @@
 function fzf_ghq_repository_select -d "Reposity selection with fzf/ghq"
-    ghq list \
-        | $FZF_CMD --exit-0 --reverse --prompt="repository> " --query "$argv" \
+    set -l selected_repo (ghq list \
+        | $FZF_CMD --reverse --no-multi \
+            --prompt="repository> " --query "$argv" \
             --scheme=path \
             --preview-window="right,65%" \
-            --preview="git -C (ghq list --exact --full-path {}) log -5 --graph --decorate --abbrev-commit --color=always" \
-        | read select
-    if test -n "$select"
-        cd (ghq list --full-path --exact $select)
+            --preview="git -C (ghq list --exact --full-path {}) log -5 --graph --decorate --abbrev-commit --color=always")
+    if test -n "$selected_repo"
+        cd (ghq list --full-path --exact $selected_repo)
     end
     commandline --function repaint
 end
