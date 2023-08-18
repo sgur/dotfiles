5c011a22ac3142477f51491a2cae8d2bbdd27338
sgur <255021+sgur@users.noreply.github.com>
2023-08-18 18:15:35 +0900

    Update .config/fish/functions/ls.fish

--- dot_config/fish/functions/ls.fish
+++ dot_config/fish/functions/ls.fish
@@ -3,7 +3,11 @@ if type -q exa
         command exa --color=auto --classify --icons $argv
     end
 else
-    if test -f /proc/sys/fs/binfmt_misc/WSLInterop
+    if type -q sw_vers
+        function ls
+            command ls --color=auto -Fh $argv
+        end
+    else if test -f /proc/sys/fs/binfmt_misc/WSLInterop
         function ls --wraps=ls --description 'ls for WSL'
             command ls --color=auto --classify --human-readable --show-control-chars --ignore="hiberfil.sys" --ignore="pagefile.sys" --ignore="swapfile.sys" --hide="bootmgr" --hide="BOOTNXT" --ignore="\$Recycle.Bin" --ignore="NTUSER.DAT*" --ignore="ntuser.*" --hide="\$tf" $argv
         end
