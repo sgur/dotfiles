9b27e9805b6a97c1c0c1bd9d9a2270e50f80c2f7
sgur <255021+sgur@users.noreply.github.com>
2023-09-06 13:40:12 +0900

    Update .config/fish/conf.d/aws-vault.fish

--- dot_config/fish/conf.d/aws-vault.fish
+++ dot_config/fish/conf.d/aws-vault.fish
@@ -1,4 +1,4 @@
-if type -q aws-vault
+if type -q aws-vault ;and type -q gopass
     set -gx AWS_VAULT_BACKEND pass
     set -gx AWS_VAULT_PASS_CMD gopass
     set -gx AWS_VAULT_PASS_PREFIX aws-vault
