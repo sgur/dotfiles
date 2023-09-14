1e4ad075f5d3505475401dcd0089fe5c66ec1228
sgur <255021+sgur@users.noreply.github.com>
2023-09-14 15:04:21 +0900

    Update .config/fish/conf.d/00-wsl.fish

--- dot_config/fish/conf.d/00-wsl.fish
+++ dot_config/fish/conf.d/00-wsl.fish
@@ -19,29 +19,29 @@ end
 #     session required pam_limits.so を追加した方がよいかも
 ulimit --file-descriptor-count 2048
 
-if status is-login
-   # https://specifications.freedesktop.org/basedir-spec/latest/ar01s03.html
-   set -gx XDG_DATA_HOME $HOME/.local/share
-   set -gx XDG_CONFIG_HOME $HOME/.config
-   set -gx XDG_DATA_DIRS /usr/local/share/:/usr/share/
-   set -gx XDG_CONFIG_DIRS /etc/xdg
-   set -gx XDG_CACHE_HOME $HOME/.cache
+status is-login || exit
 
-   # https://dev.to/bowmanjd/using-podman-on-windows-subsystem-for-linux-wsl-58ji
-   if test -z "$XDG_RUNTIME_DIR"
-      set -gx XDG_RUNTIME_DIR /run/user/$UID
+# https://specifications.freedesktop.org/basedir-spec/latest/ar01s03.html
+set -gx XDG_DATA_HOME $HOME/.local/share
+set -gx XDG_CONFIG_HOME $HOME/.config
+set -gx XDG_DATA_DIRS /usr/local/share/:/usr/share/
+set -gx XDG_CONFIG_DIRS /etc/xdg
+set -gx XDG_CACHE_HOME $HOME/.cache
+
+# https://dev.to/bowmanjd/using-podman-on-windows-subsystem-for-linux-wsl-58ji
+if test -z "$XDG_RUNTIME_DIR"
+   set -gx XDG_RUNTIME_DIR /run/user/$UID
+   if test ! -d "$XDG_RUNTIME_DIR"
+      set -gx XDG_RUNTIME_DIR /tmp/$USER-runtime
       if test ! -d "$XDG_RUNTIME_DIR"
-         set -gx XDG_RUNTIME_DIR /tmp/$USER-runtime
-         if test ! -d "$XDG_RUNTIME_DIR"
-            mkdir -m 0700 "$XDG_RUNTIME_DIR"
-         end
+         mkdir -m 0700 "$XDG_RUNTIME_DIR"
       end
    end
+end
 
-   # Disable DISPLAY
-   if test -n "$WSL2_GUI_APPS_ENABLED"
-      set -e DISPLAY
-      set -e WAYLAND_DISPLAY
-      set -e PULSE_SERVER
-   end
+# Disable DISPLAY
+if test -n "$WSL2_GUI_APPS_ENABLED"
+   set -e DISPLAY
+   set -e WAYLAND_DISPLAY
+   set -e PULSE_SERVER
 end
