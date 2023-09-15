73ba19ca1b5b9012e785fa8c8eb86073d3bf1f4b
sgur <255021+sgur@users.noreply.github.com>
2023-09-15 20:55:59 +0900

    Update .config/fish/conf.d/00-wsl.fish Update .config/fish/conf.d/10-config.fish

--- dot_config/fish/conf.d/00-wsl.fish
+++ dot_config/fish/conf.d/00-wsl.fish
@@ -5,12 +5,12 @@
 # 環境変数 $WSLENV を見るパターンでもよい
 # if test -z "$WSLENV"
 if test ! -f /proc/sys/fs/binfmt_misc/WSLInterop
-   exit 0
+    exit 0
 end
 
 # umask の設定を非WSL環境と同じにする
-if test (umask) = "0000"
-   umask 022
+if test (umask) = 0000
+    umask 022
 end
 
 # Error: Too many open files 対策
@@ -21,27 +21,9 @@ ulimit --file-descriptor-count 2048
 
 status is-login || exit
 
-# https://specifications.freedesktop.org/basedir-spec/latest/ar01s03.html
-set -gx XDG_DATA_HOME $HOME/.local/share
-set -gx XDG_CONFIG_HOME $HOME/.config
-set -gx XDG_DATA_DIRS /usr/local/share/:/usr/share/
-set -gx XDG_CONFIG_DIRS /etc/xdg
-set -gx XDG_CACHE_HOME $HOME/.cache
-
-# https://dev.to/bowmanjd/using-podman-on-windows-subsystem-for-linux-wsl-58ji
-if test -z "$XDG_RUNTIME_DIR"
-   set -gx XDG_RUNTIME_DIR /run/user/$UID
-   if test ! -d "$XDG_RUNTIME_DIR"
-      set -gx XDG_RUNTIME_DIR /tmp/$USER-runtime
-      if test ! -d "$XDG_RUNTIME_DIR"
-         mkdir -m 0700 "$XDG_RUNTIME_DIR"
-      end
-   end
-end
-
 # Disable DISPLAY
 if test -n "$WSL2_GUI_APPS_ENABLED"
-   set -e DISPLAY
-   set -e WAYLAND_DISPLAY
-   set -e PULSE_SERVER
+    set -e DISPLAY
+    set -e WAYLAND_DISPLAY
+    set -e PULSE_SERVER
 end
--- dot_config/fish/conf.d/10-config.fish
+++ dot_config/fish/conf.d/10-config.fish
@@ -5,32 +5,6 @@ for dir_path in ~/.yarn/bin ~/.cargo/bin ~/go/bin
 end
 fish_add_path -P ~/bin ~/.local/bin
 
-if status is-login
-    set -gx EDITOR vim
-    # version が 23.05 より新しければ EDITOR に hx を設定する
-    if type -q hx
-        set -l hx_version (hx --version | string split ' ')
-        if test $hx_version[2] -ge 23.05 ;and not string match '* (7f5940be)' $hx_version[3]
-            set -gx EDITOR hx
-        end
-    end
-    # git commit では copilot を使いたいので vim を利用する
-    set -gx GIT_EDITOR vim
-    if test "$TERM_PROGRAM" = 'vscode'
-        set -gx EDITOR 'code -w'
-    end
-
-    if test (uname) = 'Darwin' -a "$TERM" = 'screen-256color'
-        set -gx TERM xterm-256color
-    end
-
-    set -gx LANG ja_JP.UTF-8
-
-    set -gx PIPENV_VENV_IN_PROJECT 1
-
-    set -gx LESS --incsearch --quit-if-one-screen --raw-control-chars
-end
-
 if type -q gpg
     set -gx GPG_TTY (tty)
     gpg-connect-agent updatestartuptty /bye >/dev/null
@@ -45,3 +19,46 @@ if status is-interactive
     set sponge_purge_only_on_exit true
 end
 
+status is-login || exit
+
+set -gx EDITOR vim
+# version が 23.05 より新しければ EDITOR に hx を設定する
+if type -q hx
+    set -l hx_version (hx --version | string split ' ')
+    if test $hx_version[2] -ge 23.05; and not string match '* (7f5940be)' $hx_version[3]
+        set -gx EDITOR hx
+    end
+end
+# git commit では copilot を使いたいので vim を利用する
+set -gx GIT_EDITOR vim
+if test "$TERM_PROGRAM" = vscode
+    set -gx EDITOR 'code -w'
+end
+
+if test (uname) = Darwin -a "$TERM" = screen-256color
+    set -gx TERM xterm-256color
+end
+
+set -gx LANG ja_JP.UTF-8
+
+set -gx PIPENV_VENV_IN_PROJECT 1
+
+set -gx LESS --incsearch --quit-if-one-screen --raw-control-chars
+
+# https://specifications.freedesktop.org/basedir-spec/latest/ar01s03.html
+set -gx XDG_DATA_HOME $HOME/.local/share
+set -gx XDG_CONFIG_HOME $HOME/.config
+set -gx XDG_DATA_DIRS /usr/local/share/:/usr/share/
+set -gx XDG_CONFIG_DIRS /etc/xdg
+set -gx XDG_CACHE_HOME $HOME/.cache
+
+# https://dev.to/bowmanjd/using-podman-on-windows-subsystem-for-linux-wsl-58ji
+if test -z "$XDG_RUNTIME_DIR"
+    set -gx XDG_RUNTIME_DIR /run/user/$UID
+    if test ! -d "$XDG_RUNTIME_DIR"
+        set -gx XDG_RUNTIME_DIR /tmp/$USER-runtime
+        if test ! -d "$XDG_RUNTIME_DIR"
+            mkdir -m 0700 "$XDG_RUNTIME_DIR"
+        end
+    end
+end
