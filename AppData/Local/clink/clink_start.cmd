96dd880346b2f58454399979b70b1b958da521c7
sgur <255021+sgur@users.noreply.github.com>
2023-09-19 12:49:50 +0900

    Update AppData/Local/clink/config.lua Update .chezmoiexternal.toml Change attributes of AppData/Local/clink/fnm.lua Change attributes of AppData/Local/clink/starship.lua Change attributes of AppData/Local/clink/zoxide.lua Change attributes of AppData/Local/clink/clink_start.cmd Update AppData/Local/clink/clink_start.cmd Update AppData/Local/clink/fnm.lua Update AppData/Local/clink/starship.lua

--- AppData/Local/clink/clink_start.cmd
+++
@@ -1,4 +0,0 @@
-@REM このファイルは %LocalAppData%\clink に保存する
-@echo off
-
-@doskey ls=ls.exe -F --ignore="NTUSER.DAT*" --ignore="My Documents" --ignore="Application Data" --ignore="Local Settings" $*
--- AppData/Local/clink/config.lua
+++ AppData/Local/clink/config.lua
@@ -5,5 +5,12 @@ local scoop_git_dir = path.join(path.join(path.join(scoop_dir, "apps"), "git"), 
 
 if os.isdir(scoop_git_dir) then
   local bin_dir = path.join(path.join(scoop_git_dir, "usr"), "bin")
-  os.setenv("PATH", os.getenv("PATH") .. ";" .. bin_dir)
+  os.setenv("PATH", bin_dir .. ";" .. os.getenv("PATH"))
+end
+
+local local_dir = path.join(os.getenv("USERPROFILE"), ".local")
+local local_bin_dir = path.join(local_dir, "bin")
+
+if os.isdir(local_bin_dir) then
+  os.setenv("PATH", local_bin_dir .. ";" .. os.getenv("PATH") )
 end
