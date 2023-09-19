054899c9c8b710fad9bcbb71fb27afed6ea846db
sgur <255021+sgur@users.noreply.github.com>
2023-09-19 11:47:36 +0900

    Update .config/fish/functions/hx.fish Update .config/fish/functions/kak.fish

--- dot_config/fish/functions/hx.fish
+++ dot_config/fish/functions/hx.fish
@@ -2,7 +2,7 @@
 type -q hx; or exit 0
 
 function hx --wraps=hx
-    set -l lsp_dirs = ~/.local/share/vim-lsp-settings/servers/*
-    set -l path $PATH $lsp_dirs
+    set -l lsp_dirs ~/.local/share/vim-lsp-settings/servers/*
+    set -l path $lsp_dirs $PATH
     PATH=$path SHELL=(which bash) command hx $argv
 end
