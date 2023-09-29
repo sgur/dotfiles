58b9252e8f69d5323f8c2ca01465e1cb5a6b8086
sgur <255021+sgur@users.noreply.github.com>
2023-09-29 13:50:40 +0900

    Update README.md Update .config/fish/conf.d/30-aws-vault.fish Remove .local/bin/pass

--- dot_config/fish/conf.d/30-aws-vault.fish
+++ dot_config/fish/conf.d/30-aws-vault.fish
@@ -1,5 +1,9 @@
-if type -q aws-vault ;and type -q gopass
+if type -q aws-vault; and test -d ~/.password-store
     set -gx AWS_VAULT_BACKEND pass
-    set -gx AWS_VAULT_PASS_CMD gopass
     set -gx AWS_VAULT_PASS_PREFIX aws-vault
+
+    set -gx AWS_VAULT_PASS_CMD pass
+    if type -q gopass
+        set -gx AWS_VAULT_PASS_CMD gopass
+    end
 end
