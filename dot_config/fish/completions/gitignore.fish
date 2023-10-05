adf5dc0834b775d7a35974b7a201785de426b017
sgur <255021+sgur@users.noreply.github.com>
2023-10-05 16:39:23 +0900

    Update .config/fish/functions/gitignore.fish Add .config/fish/completions/gitignore.fish

---
+++ dot_config/fish/completions/gitignore.fish
@@ -0,0 +1,1 @@
+complete -f -c gitignore -a "(curl -sL https://www.gitignore.io/api/list | string split ',')"
--- dot_config/fish/functions/gitignore.fish
+++ dot_config/fish/functions/gitignore.fish
@@ -1,7 +1,7 @@
 function gitignore --description 'Generate a .gitignore file from gitignore.io'
-	if count $argv > /dev/null
-		curl -L -s https://www.gitignore.io/api/$argv
-	else
-		curl -L -s https://www.gitignore.io/api/list
-	end
+    if count $argv >/dev/null
+        curl -L -s https://www.gitignore.io/api/$argv
+    else
+        curl -L -s https://www.gitignore.io/api/list
+    end
 end
