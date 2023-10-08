4cca6ce4c8bd0b18b7623fc433d4a02e9afed42a
sgur <255021+sgur@users.noreply.github.com>
2023-10-09 02:53:59 +0900

    Update .config/fish/conf.d/10-rtx.fish Remove .config/fish/conf.d/30-aws-vault.fish Add .config/fish/conf.d/30-aws-vault.fish Remove .config/fish/conf.d/30-vscode.fish Add .config/fish/conf.d/30-vscode.fish Remove .config/fish/conf.d/99-debian.fish Add .config/fish/conf.d/99-debian.fish Change attributes of .config/fish/conf.d/00-wsl.fish Change attributes of .config/fish/conf.d/10-fnm.fish Change attributes of .config/fish/conf.d/30-bat.fish Change attributes of .config/fish/conf.d/30-fzf.fish Change attributes of .config/fish/conf.d/99-update-motd.fish

--- dot_config/fish/conf.d/30-aws-vault.fish
+++
@@ -1,9 +0,0 @@
-if type -q aws-vault; and test -d ~/.password-store
-    set -gx AWS_VAULT_BACKEND pass
-    set -gx AWS_VAULT_PASS_PREFIX aws-vault
-
-    set -gx AWS_VAULT_PASS_CMD pass
-    if type -q gopass
-        set -gx AWS_VAULT_PASS_CMD gopass
-    end
-end
