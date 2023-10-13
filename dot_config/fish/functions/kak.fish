ab5ee2e641fbd1f09ae53fe5bc4042d077bab601
sgur <255021+sgur@users.noreply.github.com>
2023-10-13 22:06:26 +0900

    Update .config/fish/functions/kak.fish

--- dot_config/fish/functions/kak.fish
+++ dot_config/fish/functions/kak.fish
@@ -2,7 +2,5 @@
 type -q kak; or exit 0
 
 function kak --wraps=kak
-    set -l lsp_dirs ~/.local/share/vim-lsp-settings/servers/*
-    set -l path $lsp_dirs $PATH
-    PATH=$path SHELL=(which bash) command kak $argv
+    SHELL=(which bash) command kak $argv
 end
