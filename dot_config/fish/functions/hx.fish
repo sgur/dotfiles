0c390ab3d8943161f212759b36f7f323a6f469c5
sgur <255021+sgur@users.noreply.github.com>
2023-10-16 11:47:33 +0900

    Remove .config/fish/functions/hx.fish Remove .config/fish/functions/kak.fish

--- dot_config/fish/functions/hx.fish
+++
@@ -1,6 +0,0 @@
-# helix editor
-type -q hx; or exit 0
-
-function hx --wraps=hx
-    SHELL=(which bash) command hx $argv
-end
--- dot_config/fish/functions/kak.fish
+++
@@ -1,6 +0,0 @@
-# kakoune editor
-type -q kak; or exit 0
-
-function kak --wraps=kak
-    SHELL=(which bash) command kak $argv
-end
