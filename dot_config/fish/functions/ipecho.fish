e629c21c35383b4ec4a1afc13f6e31ae96add7aa
sgur <255021+sgur@users.noreply.github.com>
2023-11-06 19:28:23 +0900

    Update .config/fish/config.fish Remove .config/fish/functions/mv.fish Remove .config/fish/functions/rm.fish Remove .config/fish/functions/ipecho.fish

--- dot_config/fish/functions/ipecho.fish
+++
@@ -1,4 +0,0 @@
-# ipecho.net/plain に curl してグローバルIPアドレスを表示する
-function ipecho
-    curl --silent ipecho.net/plain; and echo
-end
--- dot_config/fish/functions/mv.fish
+++
@@ -1,5 +0,0 @@
-function mv --wraps=mv --description 'mv -i'
-    command mv -i $argv
-end
-
-# vim:ts=4 sw=4 et
