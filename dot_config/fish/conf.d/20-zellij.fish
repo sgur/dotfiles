d5045fd332c608d4a4de0569912901701d6c6f88
sgur <255021+sgur@users.noreply.github.com>
2023-11-27 09:28:20 +0900

    Update .config/fish/conf.d/20-zellij.fish

--- dot_config/fish/conf.d/20-zellij.fish
+++ dot_config/fish/conf.d/20-zellij.fish
@@ -2,15 +2,19 @@ type -q zellij || exit
 
 function __zellij_hook --on-variable PWD
     set -l is_inside_work_tree (git rev-parse --is-inside-work-tree 2> /dev/null)
-    if test $status -ne 0
-        return
-    end
-    if $is_inside_work_tree = true
+
+    if test $status -eq 0 && $is_inside_work_tree = true
         set -l repo_name (basename (git rev-parse --show-toplevel))
         if test "$ZELLIJ_TAB_NAME" != "$repo_name"
             set -gx ZELLIJ_TAB_NAME $repo_name
             zellij action rename-tab $repo_name
         end
+        return
+    end
+
+    if test -n "$ZELLIJ_TAB_NAME"
+        zellij action undo-rename-tab
+        set -e ZELLIJ_TAB_NAME
     end
 end
 __zellij_hook
