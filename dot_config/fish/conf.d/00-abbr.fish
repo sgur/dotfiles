78ea9ee6ffb627d0b67ebf6fe10e22a3ff279bd8
sgur <255021+sgur@users.noreply.github.com>
2023-12-05 15:38:25 +0900

    Update .config/fish/conf.d/00-abbr.fish

--- dot_config/fish/conf.d/00-abbr.fish
+++ dot_config/fish/conf.d/00-abbr.fish
@@ -25,3 +25,5 @@ abbr --add aws.e --set-cursor "aws-vault exec % -- "
 abbr --add aws.l --set-cursor "aws-vault login % --stdout | fish_clipboard_copy"
 
 abbr --add pwsh /mnt/c/Program\\ Files/PowerShell/7/pwsh.exe
+
+abbr --add hostname uname -n
