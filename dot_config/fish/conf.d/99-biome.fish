54e3a19abdf485acce5d8ded0a0287f859d93f6f
sgur <255021+sgur@users.noreply.github.com>
2024-01-11 18:03:22 +0900

    Add .config/fish/conf.d/99-biome.fish

---
+++ dot_config/fish/conf.d/99-biome.fish
@@ -0,0 +1,8 @@
+function enable_biome_default_config --on-event fish_preexec
+    set -l toplevel (git rev-parse --show-toplevel 2> /dev/null)
+    if test \( $status -eq 0 \) -a \( -e "$toplevel/biome.json" \)
+        set -e BIOME_CONFIG_PATH
+    else
+        set -gx BIOME_CONFIG_PATH ~/.config/biome
+    end
+end
