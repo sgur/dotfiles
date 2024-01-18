a70f8c453044fa0510c54057e3c7057f7ec5913d
sgur <255021+sgur@users.noreply.github.com>
2024-01-18 10:59:35 +0900

    Update Documents/PowerShell/Microsoft.PowerShell_profile.ps1

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -381,7 +381,7 @@ $CoreutilsBin = @("basename", "cat", "chmod", "comm", "cp", "cut", "cygpath",
 	"date", "diff", "dirname", "echo", "env", "expr", "false", "fold", "grep",
 	"head", "id", "install", "join", "ln", "ls", "md5sum", "mkdir", "mv", "od",
 	"paste", "printf", "ps", "pwd", "rm", "rmdir", "sleep", "sort", "split", "stty",
-	"tail", "tee", "touch", "tr", "true", "uname", "uniq", "wc")
+	"tail", "tee", "touch", "tr", "true", "uname", "uniq", "wc", "iconv")
 # "msysmnt" is excluded from coreutils
 $ReadonlyBin = @("tee", "diff", "ls", "sleep", "sort")
 $ConfirmationRequiredBin = @("cp", "mv")
