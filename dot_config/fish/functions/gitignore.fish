cc4a521c67b2216bbec61fd4d5b10768dacf4576
sgur <255021+sgur@users.noreply.github.com>
2024-01-29 10:11:09 +0900

    Update .config/fish/functions/gitignore.fish

--- dot_config/fish/functions/gitignore.fish
+++ dot_config/fish/functions/gitignore.fish
@@ -2,6 +2,6 @@ function gitignore --description 'Generate a .gitignore file from gitignore.io'
     if count $argv >/dev/null
         curl -L -s https://www.gitignore.io/api/$argv
     else
-        curl -L -s https://www.gitignore.io/api/list
+        curl -L -s https://www.gitignore.io/api/list | string join ',' | string replace -a ',' ' ' | fold --spaces
     end
 end
