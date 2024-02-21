c263e2a4441825e8e105c53be680768a24f853d0
sgur <255021+sgur@users.noreply.github.com>
2024-02-21 14:27:43 +0900

    Update .config/fish/functions/fzf_zoxide_select.fish

--- dot_config/fish/functions/fzf_zoxide_select.fish
+++ dot_config/fish/functions/fzf_zoxide_select.fish
@@ -2,7 +2,7 @@ function fzf_zoxide_select
     set -l dir (zoxide query --list \
         | $FZF_CMD --reverse --no-multi --prompt="zoxide> " --query "$argv" \
         --scheme=path \
-        --preview-window="right,50%" \
+        --preview-window="right,40%" \
         --preview='ls -1 {}')
     if test -n "$dir"
         cd "$dir"
