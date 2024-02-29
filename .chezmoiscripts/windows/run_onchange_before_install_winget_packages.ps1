51c48ecdac3bb708a53aafc53feddac200d2f46f
sgur <255021+sgur@users.noreply.github.com>
2024-02-29 18:58:10 +0900

    Update .chezmoiscripts/windows/install_winget_packages.ps1

--- .chezmoiscripts/windows/run_onchange_before_install_winget_packages.ps1
+++ .chezmoiscripts/windows/run_onchange_before_install_winget_packages.ps1
@@ -1,6 +1,20 @@
-winget pin add Microsoft.Edge
-winget pin add Microsoft.Office
-winget pin add Microsoft.EdgeWebView2Runtime
 winget pin add "Microsoft Edge Update"
+winget pin add --id Microsoft.Edge
+winget pin add --id Microsoft.EdgeWebView2Runtime
+winget pin add --id Microsoft.Office
+winget pin add --id Microsoft.OneDrive
 
-winget install --no-upgrade GnuPG.Gpg4win
+# From winget source
+winget install --no-upgrade --id GnuPG.Gpg4win
+winget install --no-upgrade --id HermannSchinagl.LinkShellExtension
+winget install --no-upgrade --id Microsoft.PowerShell
+winget install --no-upgrade --id Microsoft.PowerToys
+winget install --no-upgrade --id Microsoft.VisualStudio.2022.Professional
+winget install --no-upgrade --id Microsoft.VisualStudioCode
+winget install --no-upgrade --id Mozilla.Firefox.DeveloperEdition
+winget install --no-upgrade --id nathancorvussolis.corvusskk
+
+# From msstore
+winget install --no-upgrade DevToys
+winget install --no-upgrade Ubuntu
+
