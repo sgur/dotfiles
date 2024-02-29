037dc421c8fdb9973a5374989d0323a16a27fd4b
sgur <255021+sgur@users.noreply.github.com>
2024-02-29 14:10:13 +0900

    Update .chezmoiscripts/windows/install_winget_packages.ps1 Update scoop/persist/windows-terminal-preview/settings/settings.json

--- .chezmoiscripts/windows/run_onchange_before_install_winget_packages.ps1
+++ .chezmoiscripts/windows/run_onchange_before_install_winget_packages.ps1
@@ -3,10 +3,4 @@ winget pin add Microsoft.Office
 winget pin add Microsoft.EdgeWebView2Runtime
 winget pin add "Microsoft Edge Update"
 
-winget install GnuPG.GnuPG
-winget install GnuPG.Gpg4win
-
-winget install Microsoft.DotNet.DesktopRuntime.8
-winget install Microsoft.DotNet.AspNetCore.8
-winget install Microsoft.DotNet.SDK.8
-
+winget install --no-upgrade GnuPG.Gpg4win
