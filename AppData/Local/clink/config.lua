7b2a53d73a3f32a24ebfa2ec7ef70e51d496d883
sgur <255021+sgur@users.noreply.github.com>
2024-04-04 17:30:15 +0900

    Update AppData/Local/clink/config.lua

--- AppData/Local/clink/config.lua
+++ AppData/Local/clink/config.lua
@@ -14,3 +14,9 @@ local local_bin_dir = path.join(local_dir, "bin")
 if os.isdir(local_bin_dir) then
   os.setenv("PATH", local_bin_dir .. ";" .. os.getenv("PATH") )
 end
+
+local proto_home = path.join(os.getenv("USERPROFILE"), ".proto")
+if os.isdir(proto_home) then
+  os.setenv("PROTO_HOME", proto_home)
+  os.setenv("PATH", path.join(proto_home, "shims") .. ";" .. path.join(proto_home, "bin") .. ";" .. os.getenv("PATH") )
+end
