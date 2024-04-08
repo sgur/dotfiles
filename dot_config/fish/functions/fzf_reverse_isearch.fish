dd2524375cc26b83bde1dc32299ef551ec2d4e75
sgur <255021+sgur@users.noreply.github.com>
2024-04-08 10:18:42 +0900

    Remove .config/fish/functions/fzf_reverse_isearch.fish

--- dot_config/fish/functions/fzf_reverse_isearch.fish
+++
@@ -1,6 +0,0 @@
-function fzf_reverse_isearch --description 'Search history'
-    history merge
-    history | $FZF_CMD --reverse --prompt="history> " --scheme=history --tiebreak=index --query "$argv" | read select
-    and commandline -- $select
-    commandline -f repaint
-end
