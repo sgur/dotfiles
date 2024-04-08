e285ac8764bbb12fb9fc9bc85384b0e225ba067b
sgur <255021+sgur@users.noreply.github.com>
2024-04-08 13:30:48 +0900

    Update .config/fish/functions/fzf_ghq_repository_select.fish

--- dot_config/fish/functions/fzf_ghq_repository_select.fish
+++ dot_config/fish/functions/fzf_ghq_repository_select.fish
@@ -1,8 +1,11 @@
 function fzf_ghq_repository_select -d "Reposity selection with fzf/ghq"
+    test -n "$FZF_TMUX_HEIGHT"; or set FZF_TMUX_HEIGHT 40%
     set -l selected_repo (ghq list \
-        | $FZF_CMD --reverse --no-multi \
-            --prompt="repository> " --query "$argv" \
+        | $FZF_CMD --height $FZF_TMUX_HEIGHT \
             --scheme=path \
+            --bind=ctrl-z:ignore \
+            --no-multi \
+            --prompt="repository> " --query "$argv" \
             --preview-window="right,60%" \
             --preview="git -C (ghq list --exact --full-path {}) log -5 --graph --decorate --abbrev-commit --color=always")
     if test -n "$selected_repo"
