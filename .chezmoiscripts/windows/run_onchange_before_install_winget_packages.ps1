b31f73daca5e2f2c023eb0d09fc9ecf396241ddc
sgur <255021+sgur@users.noreply.github.com>
2024-04-11 11:34:54 +0900

    Update .chezmoiscripts/windows/install_winget_packages.ps1

--- .chezmoiscripts/windows/run_onchange_before_install_winget_packages.ps1
+++ .chezmoiscripts/windows/run_onchange_before_install_winget_packages.ps1
@@ -6,16 +6,17 @@ winget pin add --id Microsoft.OneDrive
 winget pin add --id SlackTechnologies.Slack
 
 # From winget source
-winget install --no-upgrade --id GnuPG.Gpg4win
-winget install --no-upgrade --id HermannSchinagl.LinkShellExtension
-winget install --no-upgrade --id Microsoft.PowerShell
-winget install --no-upgrade --id Microsoft.PowerToys
-winget install --no-upgrade --id Microsoft.VisualStudio.2022.Professional
-winget install --no-upgrade --id Microsoft.VisualStudioCode
-winget install --no-upgrade --id Mozilla.Firefox.DeveloperEdition
-winget install --no-upgrade --id nathancorvussolis.corvusskk
+winget install --no-upgrade --source winget --id GnuPG.Gpg4win
+winget install --no-upgrade --source winget --id HermannSchinagl.LinkShellExtension
+winget install --no-upgrade --source winget --id Microsoft.PowerShell
+winget install --no-upgrade --source winget --id Microsoft.PowerToys
+winget install --no-upgrade --source winget --id Microsoft.VisualStudio.2022.Professional
+winget install --no-upgrade --source winget --id Microsoft.VisualStudioCode
+winget install --no-upgrade --source winget --id Mozilla.Firefox.DeveloperEdition
+winget install --no-upgrade --source winget --id nathancorvussolis.corvusskk
 
 # From msstore
-winget install --no-upgrade --name 'Microsoft PC Manager'
-winget install --no-upgrade --name --exact Ubuntu
-winget install --no-upgrade --name DevToys
+winget install --no-upgrade --source msstore --name 'Microsoft PC Manager'
+winget install --no-upgrade --source msstore --name 'Microsoft To Do'
+winget install --no-upgrade --source msstore --name --exact Ubuntu
+winget install --no-upgrade --source msstore --name DevToys
