16d2c8c8599493546b43ec9684a70223405b740c
sgur <255021+sgur@users.noreply.github.com>
2024-05-14 16:25:46 +0900

    Update .config/fish/conf.d/30-vscode.fish

--- dot_config/fish/conf.d/30-vscode.fish
+++ dot_config/fish/conf.d/30-vscode.fish
@@ -5,13 +5,13 @@ status is-interactive || exit
 
 string match -q "$TERM_PROGRAM" "vscode" || exit
 
-set -l $shell_integration ~/.vscode-server/bin/*/out/vs/workbench/contrib/terminal/browser/media/fish_xdg_data/fish/vendor_conf.d/shellIntegration.fish
+set -l shell_integration ~/.vscode-server/bin/*/out/vs/workbench/contrib/terminal/browser/media/fish_xdg_data/fish/vendor_conf.d/shellIntegration.fish
 if test -f $shell_integration
     source $shell_integration
     exit
 end
 
-set -l $old_shell_integration ~/.vscode-server/bin/*/out/vs/workbench/contrib/terminal/browser/media/shellIntegration.fish
+set -l old_shell_integration ~/.vscode-server/bin/*/out/vs/workbench/contrib/terminal/browser/media/shellIntegration.fish
 if test -f $old_shell_integration
     source $old_shell_integration
 end
