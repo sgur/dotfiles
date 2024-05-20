60644072a50508d7020af4376b21af2f333037f1
sgur <255021+sgur@users.noreply.github.com>
2024-05-20 12:09:28 +0900

    Update .config/fish/conf.d/99-biome.fish

--- dot_config/fish/conf.d/99-biome.fish
+++ dot_config/fish/conf.d/99-biome.fish
@@ -1,6 +1,6 @@
 function enable_biome_default_config --on-event fish_preexec
     set -l toplevel (git rev-parse --show-toplevel 2> /dev/null)
-    if test \( $status -eq 0 \) -a \( -e "$toplevel/biome.json" \)
+    if test \( $status -eq 0 \) -a \( -e "$toplevel/biome.json" -o -e "$toplevel/biome.jsonc" \)
         set -e BIOME_CONFIG_PATH
     else
         set -gx BIOME_CONFIG_PATH ~/.config/
