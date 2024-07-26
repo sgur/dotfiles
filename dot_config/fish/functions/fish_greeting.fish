0a23eb23cdd50d2f9f8fa1a2ba99b9ec8e90eb15
sgur <255021+sgur@users.noreply.github.com>
2024-07-26 12:59:29 +0900

    Update .config/fish/functions/fish_greeting.fish

--- dot_config/fish/functions/fish_greeting.fish
+++ dot_config/fish/functions/fish_greeting.fish
@@ -1,10 +1,14 @@
 function fish_greeting
-    echo -n '🐟, the friendly'
+    echo -n 'Welcome to 🐟, the friendly'
     if status is-login
         echo -n ", login"
     end
     if status is-interactive
         echo -n ", interactive"
     end
-    echo " shell"
+    echo -n " shell"
+    if test -z "$fish_prompt_type" && type -q tide
+        echo -n ' with 🌊 Tide'
+    end
+    echo ""
 end
