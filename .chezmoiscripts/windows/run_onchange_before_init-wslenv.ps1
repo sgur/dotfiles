b85e78d282e492db48ee7e0bde57634408509b51
sgur <255021+sgur@users.noreply.github.com>
2024-09-06 16:11:58 +0900

    Update .chezmoiscripts/windows/init-wslenv.ps1

--- .chezmoiscripts/windows/run_onchange_before_init-wslenv.ps1
+++ .chezmoiscripts/windows/run_onchange_before_init-wslenv.ps1
@@ -11,5 +11,11 @@ if ($ENV:WSLENV -notlike "*OneDrive*") {
 	$NewEnv += 'OneDrive/p:'
 }
 
+# WSLENV に CHEZMOI_GITHUB_ACCESS_TOKEN が含まれているかチェック
+if ($ENV:WSLENV -notlike "*CHEZMOI_GITHUB_ACCESS_TOKEN*") {
+	# OneDrive が含まれていない場合は追加する
+	$NewEnv += 'CHEZMOI_GITHUB_ACCESS_TOKEN:'
+}
+
 [Environment]::SetEnvironmentVariable("WSLENV", $NewEnv, [EnvironmentVariableTarget]::Process)
 [Environment]::SetEnvironmentVariable("WSLENV", $NewEnv.Replace("WT_SESSION:", "").Replace("WT_PROFILE_ID:", ""), [EnvironmentVariableTarget]::User)
