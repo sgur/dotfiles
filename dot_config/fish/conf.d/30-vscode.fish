ee40f923af8c65aff1bd56854f66850df7e8fe83
sgur <255021+sgur@users.noreply.github.com>
2024-12-25 21:07:06 +0900

    Update .config/fish/conf.d/30-fzf.fish Update .config/fish/conf.d/30-vscode.fish

--- dot_config/fish/conf.d/30-vscode.fish
+++ dot_config/fish/conf.d/30-vscode.fish
@@ -3,15 +3,23 @@ status is-interactive || exit
 # https://code.visualstudio.com/docs/terminal/shell-integration
 # code --locate-shell-integration-path fish
 
-string match -q "$TERM_PROGRAM" "vscode" || exit
+string match -q "$TERM_PROGRAM" vscode || exit
 
-set -l shell_integration ~/.vscode-server/bin/*/out/vs/workbench/contrib/terminal/browser/media/fish_xdg_data/fish/vendor_conf.d/shellIntegration.fish
-if test -f $shell_integration
-    source $shell_integration
-    exit
-end
+test -n "$TMUX" && set -e TMUX
+test -n "$TMUX_PANE" && set -e TMUX_PANE
+test -n "$TMUX_PLUGIN_MANAGER_PATH" && set -e TMUX_PLUGIN_MANAGER_PATH
 
-set -l old_shell_integration ~/.vscode-server/bin/*/out/vs/workbench/contrib/terminal/browser/media/shellIntegration.fish
-if test -f $old_shell_integration
-    source $old_shell_integration
+test -n "$ZELLIJ" && set -e ZELLIJ
+test -n "$ZELLIJ_PANE_ID" && set -e ZELLIJ_PANE_ID
+test -n "$ZELLIJ_SESSION_NAME" && set -e ZELLIJ_SESSION_NAME
+
+set -l scripts \
+    ~/.vscode-server/bin/*/out/vs/workbench/contrib/terminal/common/scripts/shellIntegration.fish \
+    ~/.vscode-server/bin/*/out/vs/workbench/contrib/terminal/browser/media/fish_xdg_data/fish/vendor_conf.d/shellIntegration.fish \
+    ~/.vscode-server/bin/*/out/vs/workbench/contrib/terminal/browser/media/shellIntegration.fish
+for shell_integration in $scripts
+    if test -f $shell_integration
+        source $shell_integration
+        break
+    end
 end
