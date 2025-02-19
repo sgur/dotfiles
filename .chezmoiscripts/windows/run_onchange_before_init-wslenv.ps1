873dce805ac23cd4b1ea8a9b78f2217bd503ef4c
sgur <255021+sgur@users.noreply.github.com>
2025-02-19 19:33:46 +0900

    Update .chezmoiscripts/windows/init-wslenv.ps1

--- .chezmoiscripts/windows/run_onchange_before_init-wslenv.ps1
+++ .chezmoiscripts/windows/run_onchange_before_init-wslenv.ps1
@@ -1,19 +1,22 @@
 $NewEnv = $ENV:WSLENV
 # WSLENV に "USERPROFILE" が含まれているかチェックする
-if ($ENV:WSLENV -notlike "*USERPROFILE*") {
+if ($ENV:WSLENV -notlike "*USERPROFILE*")
+{
 	# OneDrive が含まれていない場合は追加する
 	$NewEnv += 'USERPROFILE/p:'
 }
 
 # WSLENV に "OneDrive" が含まれているかチェックする
-if ($ENV:WSLENV -notlike "*OneDrive*") {
+if ($ENV:WSLENV -notlike "*OneDrive*")
+{
 	# OneDrive が含まれていない場合は追加する
 	$NewEnv += 'OneDrive/p:'
 }
 
 # WSLENV に CHEZMOI_GITHUB_ACCESS_TOKEN が含まれているかチェック
-if ($ENV:WSLENV -notlike "*CHEZMOI_GITHUB_ACCESS_TOKEN*") {
-	# OneDrive が含まれていない場合は追加する
+if ($ENV:WSLENV -notlike "*CHEZMOI_GITHUB_ACCESS_TOKEN*")
+{
+	# CHEZMOI_GITHUB_ACCESS_TOKEN が含まれていない場合は追加する
 	$NewEnv += 'CHEZMOI_GITHUB_ACCESS_TOKEN:'
 }
 
