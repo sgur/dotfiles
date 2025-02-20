edcc7c1bf890705a242fcea1961a5b69b6ac619b
sgur <255021+sgur@users.noreply.github.com>
2025-02-20 19:01:11 +0900

    Add .config/fish/conf.d/99-less.fish

---
+++ dot_config/fish/conf.d/99-less.fish
@@ -0,0 +1,9 @@
+type -q less || exit 0
+
+set LESS_TERMCAP_mb (tput bold) # begin blinking
+set LESS_TERMCAP_md (tput bold; tput setaf 4) # begin bold (blue)
+set LESS_TERMCAP_me (tput sgr0) # end mode
+set LESS_TERMCAP_se (tput sgr0) # end standout-mode
+set LESS_TERMCAP_so (tput bold; tput setaf 3) # begin standout-mode (yellow)
+set LESS_TERMCAP_ue (tput rmul; tput sgr0) # end underline
+set LESS_TERMCAP_us (tput smul; tput setaf 2) # begin underline (green)
